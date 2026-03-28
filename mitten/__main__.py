1a2bb4e42daebd50acbb7c1cdb8be33630465945
mit <doctorwho0789@gmail.com>
2026-03-28 19:37:44 -0400

    fix: handle relative import failure in PyInstaller entry point (0.3.7.4)

--- mitten/__main__.py
+++ mitten/__main__.py
@@ -1,4 +1,7 @@
-"""Support: python -m mitten"""
-from .cli import main
+"""Support: python -m mitten and PyInstaller entry point"""
+try:
+    from .cli import main
+except ImportError:
+    from mitten.cli import main
 
 main()
